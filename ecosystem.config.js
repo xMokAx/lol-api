f4ff242c071c2bd76ac3901d76525ebb3250ff89
xMokAx <ahmedmokhtar11j@gmail.com>
2018-12-24 09:48:01 +0200

    setup pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [
+    {
+      name: "lolapi",
+      script: "./index.js"
+    }
+  ],
+  deploy: {
+    production: {
+      user: "ubuntu",
+      host: "ec2-18-196-101-204.eu-central-1.compute.amazonaws.com",
+      key: "~/.ssh/lol-app.pem",
+      ref: "origin/master",
+      repo: "https://git-codecommit.eu-central-1.amazonaws.com/v1/repos/lolapi",
+      path: "/home/ubuntu/lolapi",
+      "post-deploy": "yarn install && pm2 startOrRestart ecosystem.config.js"
+    }
+  }
+};
