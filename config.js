ab61c8f81b0f7bc02445cc9888983899a240d974
xMokAx <ahmedmokhtar11j@gmail.com>
2019-03-15 00:44:45 +0200

    use https with champion.gg api

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
   lolapikey: "RGAPI-fcf955a5-5b75-45e2-9175-162941a0e656",
   lolapiversion: "v4",
   season: "13",
-  ggendpoint: "http://api.champion.gg/v2/",
+  ggendpoint: "https://api.champion.gg/v2/",
   ggapikey: "1f1cabbb61c44c979853bff44c6a385f",
   port: process.env.PORT || 5000,
   appsecret: "lolgga11jm23j",
